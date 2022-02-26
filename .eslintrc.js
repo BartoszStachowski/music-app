68ad5d9592e0bf11019814b584edf1770d14702b
stachowski <stachowskibartosz96@gmail.com>
2022-02-26 12:16:00 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+ module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "indent": [2, "tab"],
+    "no-tabs": 0,
+    "no-mixed-spaces-and-tabs": 0,
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
