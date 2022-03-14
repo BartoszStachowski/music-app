818462b08a287db98643fc8a3a9b7ceca9743487
stachowski <stachowskibartosz96@gmail.com>
2022-03-14 22:45:36 +0100

    password validation

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -3,6 +3,7 @@ import {
 } from 'vee-validate';
 import {
 	required, min, max, alpha_spaces as alphaSpaces, email, min_value as minVal, max_value as maxVal,
+	confirmed,
 } from '@vee-validate/rules';
 
 export default {
@@ -18,5 +19,6 @@ export default {
 		defineRule('email', email);
 		defineRule('min_value', minVal);
 		defineRule('max_value', maxVal);
+		defineRule('confirmed', confirmed);
 	},
 };
