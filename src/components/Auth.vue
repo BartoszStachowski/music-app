e536b9fa6a525eac2ebfbb9a229935cebc69ca35
stachowski <stachowskibartosz96@gmail.com>
2022-03-14 21:57:21 +0100

    email validation

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -98,10 +98,14 @@
 						<!-- Email -->
 						<div class="mb-3">
 							<label class="inline-block mb-2">Email</label>
-							<input type="email"
-										 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+							<vee-field
+									type="email"
+									name="email"
+									class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-										 placeholder="Enter Email" />
+									placeholder="Enter Email"
+							/>
+							<ErrorMessage class="text-red-600" name="email" />
 						</div>
 						<!-- Age -->
 						<div class="mb-3">
@@ -164,7 +168,7 @@ export default {
 			tab: 'login',
 			schema: {
 				name: 'required|min:3|max:100|alpha_spaces',
-				email: '',
+				email: 'required|min:3|max:100|email',
 				age: '',
 				password: '',
 				confirm_password: '',
