ff66fea43f6ac5f5af3a65ec170f81831ebb2ab1
stachowski <stachowskibartosz96@gmail.com>
2022-03-30 22:21:33 +0200

    register

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -116,6 +116,8 @@
 </template>
 
 <script>
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
 export default {
 	name: 'RegisterForm',
 	data() {
@@ -125,7 +127,7 @@ export default {
 				name: 'required|min:3|max:100|alpha_spaces',
 				email: 'required|min:3|max:100|email',
 				age: 'required|min_value:18|max_value:100',
-				password: 'required|min:3|max:100',
+				password: 'required|min:6|max:100',
 				confirm_password: 'password_mismatch:@password',
 				country: 'required|country_excluded:Russia',
 				tos: 'tos',
@@ -140,15 +142,26 @@ export default {
 		};
 	},
 	methods: {
-		register(values) {
+		async register(values) {
 			this.reg_show_alert = true;
 			this.reg_in_submission = true;
 			this.reg_alert_variant = 'bg-blue-500';
 			this.reg_alert_msg = 'Please wait! Your account is being created.';
 
+			let userCred = null;
+			const auth = getAuth();
+			try {
+				userCred = await createUserWithEmailAndPassword(auth, values.email, values.password);
+			} catch (error) {
+				this.reg_in_submission = false;
+				this.reg_alert_variant = 'bg-red-500';
+				this.reg_alert_msg = 'An unexpected error occurred. Please try again later.';
+				return;
+			}
+
 			this.reg_alert_variant = 'bg-green-500';
 			this.reg_alert_msg = 'Success! Your account has been created';
-			console.log(values);
+			console.log(userCred);
 		},
 	},
 };
