74c4af4fe6d4d7741ab7aaf4d14d8a3077882000
stachowski <stachowskibartosz96@gmail.com>
2022-04-10 13:55:58 +0200

    register refactor

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -116,8 +116,6 @@
 </template>
 
 <script>
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth, usersCollection } from '@/includes/firebase';
 
 export default {
 	name: 'RegisterForm',
@@ -149,22 +147,8 @@ export default {
 			this.reg_alert_variant = 'bg-blue-500';
 			this.reg_alert_msg = 'Please wait! Your account is being created.';
 
-			let userCred = null;
 			try {
-				userCred = await createUserWithEmailAndPassword(auth, values.email, values.password);
-			} catch (error) {
-				this.reg_in_submission = false;
-				this.reg_alert_variant = 'bg-red-500';
-				this.reg_alert_msg = 'An unexpected error occurred. Please try again later.';
-				return;
-			}
-			try {
-				await usersCollection({
-					name: values.name,
-					email: values.email,
-					age: values.age,
-					country: values.country,
-				});
+				await this.$store.dispatch('register', values);
 			} catch (error) {
 				this.reg_in_submission = false;
 				this.reg_alert_variant = 'bg-red-500';
@@ -174,7 +158,6 @@ export default {
 
 			this.reg_alert_variant = 'bg-green-500';
 			this.reg_alert_msg = 'Success! Your account has been created';
-			console.log(userCred);
 		},
 	},
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,35 @@
 import { createStore } from 'vuex';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, usersCollection } from '@/includes/firebase';
 
 export default createStore({
 	state: {
 		authModalShow: false,
+		userLoggedIn: false,
 	},
 	mutations: {
 		toggleAuthModal: (state) => {
 			state.authModalShow = !state.authModalShow;
 		},
+		toggleAuth(state) {
+			state.userLoggedIn = !state.userLoggedIn;
+		},
 	},
 	getters: {
 		// authModalShow: (state) => state.authModalShow,
+	},
+	actions: {
+		async register({ commit }, payload) {
+			await createUserWithEmailAndPassword(auth, payload.email, payload.password);
+
+			await usersCollection({
+				name: payload.name,
+				email: payload.email,
+				age: payload.age,
+				country: payload.country,
+			});
+
+			commit('toggleAuth');
+		},
 	},
 });
